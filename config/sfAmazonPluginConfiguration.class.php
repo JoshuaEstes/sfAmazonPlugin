5f22d47ea208bd849b602d7ee37721b8de4e4286
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2010-12-30 11:41:44 -0500

    first commit I think, not too sure

---
+++ config/sfAmazonPluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * sfAmazonPlugin configuration.
+ * 
+ * @package     sfAmazonPlugin
+ * @subpackage  config
+ * @author      Your name here
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class sfAmazonPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
