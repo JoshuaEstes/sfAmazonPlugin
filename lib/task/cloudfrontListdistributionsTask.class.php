3a239d5e1920bd7d251cc0b8b29ba62dd585f596
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2011-01-03 15:33:49 -0500

    added more empty tasks

---
+++ lib/task/cloudfrontListdistributionsTask.class.php
@@ -0,0 +1,38 @@
+<?php
+
+class cloudfrontListdistributionsTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    // // add your own arguments here
+    // $this->addArguments(array(
+    //   new sfCommandArgument('my_arg', sfCommandArgument::REQUIRED, 'My argument'),
+    // ));
+
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'doctrine'),
+      // add your own options here
+    ));
+
+    $this->namespace        = 'cloudfront';
+    $this->name             = 'list-distributions';
+    $this->briefDescription = '';
+    $this->detailedDescription = <<<EOF
+The [cloudfront:list-distributions|INFO] task does things.
+Call it with:
+
+  [php symfony cloudfront:list-distributions|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    // initialize the database connection
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+
+    // add your code here
+  }
+}
